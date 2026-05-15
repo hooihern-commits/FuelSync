5663c1a5ae0835fe1c29848f4f9cd9652ce764c5
hooihern <nghooihern@gmail.com>
2026-05-15 19:40:54 +0800

    set up expo router structure and API client

---
+++ client/src/api/client.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { getToken } from '../storage/token';
+
+const api = axios.create({
+  baseURL: '172.20.10.2:3000',
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = await getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
